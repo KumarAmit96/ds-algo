577118e82d104457748307488cb4646c5583dafd
Kumar96_Amit <kumar20.amitsingh@gmail.com>
2024-07-15 18:25:19 +0530

    Added the package. Also added two methods to solve this problem.

--- src/com/ds/algo/hash/SubstringWithConcatenation.java
+++ src/com/ds/algo/hash/SubstringWithConcatenation.java
@@ -1,4 +1,4 @@
-package com.ds.algo;
+package com.ds.algo.hash;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,12 +9,50 @@ public class SubstringWithConcatenation {
 
     public static void main(String[] args) {
         SubstringWithConcatenation solution = new SubstringWithConcatenation();
-        String s = "wordgoodwordgoodbestword";
-        String[] words = {"word","good","best","word"};
-        System.out.println(solution.findSubstring(s, words)); // Output: [0, 9]
+        String s = "barfoothefoobarman";
+        String[] words = {"foo","bar"};
+        System.out.println(solution.findSubstringByTypeOne(s, words));
+        System.out.println(solution.findSubstringByTypeTwo(s, words));
     }
 
-    public List<Integer> findSubstring(String s, String[] words) {
+    public List<Integer> findSubstringByTypeOne(String s, String[] words) {
+        Map<String, Integer> cnt = new HashMap<>();
+        for (String w : words) {
+            cnt.merge(w, 1, Integer::sum);
+        }
+        int m = s.length(), n = words.length;
+        int k = words[0].length();
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < k; ++i) {
+            Map<String, Integer> cnt1 = new HashMap<>();
+            int l = i, r = i;
+            int t = 0;
+            while (r + k <= m) {
+                String w = s.substring(r, r + k);
+                r += k;
+                if (!cnt.containsKey(w)) {
+                    cnt1.clear();
+                    l = r;
+                    t = 0;
+                    continue;
+                }
+                cnt1.merge(w, 1, Integer::sum);
+                ++t;
+                while (cnt1.get(w) > cnt.get(w)) {
+                    String remove = s.substring(l, l + k);
+                    l += k;
+                    cnt1.merge(remove, -1, Integer::sum);
+                    --t;
+                }
+                if (t == n) {
+                    ans.add(l);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public List<Integer> findSubstringByTypeTwo(String s, String[] words) {
         List<Integer> result = new ArrayList<>();
         if (s == null || s.isEmpty() || words == null || words.length == 0) {
             return result;
